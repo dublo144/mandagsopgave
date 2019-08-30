98d6c2d05fe70aa61797a0e35ecb95db62d56d14
Mads Brandt <mads.brandt93@gmail.com>
2019-08-30 09:18:54 +0200

    Added splash screen, might need to elaborate, closes #22

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// GOOGLE MAPS
+// Pre loader
+$(window).on('load', function () {
+  var tl = new TimelineMax();
+      tl.to(".loader1-left",1.1,{x:"-100%",ease: Expo.easeOut})
+      .to(".loader1-right",1.1,{x:"100%",ease: Expo.easeOut},"-=1.1")
+      .to(".loader2-left",.8,{x:"-100%",ease: SlowMo.ease.config(0.3, 0.7, false)},"-=.8")
+      .to(".loader2-right",.8,{x:"100%",ease: SlowMo.ease.config(0.3, 0.7, false)},"-=.8")
+      .to(".loader3-left",.5,{x:"-100%",ease: Expo.easeOut})
+      .to(".loader3-right",.5,{x:"100%",ease: Expo.easeOut},"-=.5")
+      .to(".loader4-left",.5,{x:"-100%",ease: Expo.easeOut})
+      .to(".loader4-right",.5,{x:"100%",ease: Expo.easeOut})
+});
 
+
+// GOOGLE MAPS
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
