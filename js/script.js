0c4146db0fcf8545a78999bb7e96ac5f8bde1e9a
Mads Brandt <mads.brandt93@gmail.com>
2019-08-30 16:45:53 +0200

    fixed scroll icon animation, closes #25

--- js/script.js
+++ js/script.js
@@ -14,16 +14,15 @@ $(window).on('load', function () {
 
 function fadeIn() {
   document.getElementById('page-top').style.opacity='1';
+  document.getElementById('hero-content').style.opacity='1';
   
 }
 
 // GOOGLE MAPS
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
   var cphBusiness = {lat: 55.770170, lng: 12.512299};
 
-  // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {
         zoom: 15, 
@@ -34,6 +33,5 @@ function initMap() {
         }
       });
       
-  // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: cphBusiness, map: map});
 }
